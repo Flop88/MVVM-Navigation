3c74ae032a022d5b9175335b937cef44e754af50
Flop88 <wwwqwwwqwwwqwwwq@gmail.com>
2020-10-30 10:21:59 +0300

    Navigation work

---
+++ app/src/main/java/ru/mvlikhachev/mvvmnavigation/FirstFragment.kt
@@ -0,0 +1,30 @@
+package ru.mvlikhachev.mvvmnavigation
+
+import android.os.Bundle
+import androidx.fragment.app.Fragment
+import android.view.LayoutInflater
+import android.view.View
+import android.view.ViewGroup
+import kotlinx.android.synthetic.main.fragment_first.*
+
+
+class FirstFragment : Fragment() {
+
+
+    override fun onCreateView(
+        inflater: LayoutInflater, container: ViewGroup?,
+        savedInstanceState: Bundle?
+    ): View? {
+        // Inflate the layout for this fragment
+        return inflater.inflate(R.layout.fragment_first, container, false)
+    }
+
+
+    override fun onStart() {
+        super.onStart()
+
+        firstNextButton.setOnClickListener {
+            (activity as MainActivity).navController.navigate(R.id.action_firstFragment_to_secondFragment)
+        }
+    }
+}
